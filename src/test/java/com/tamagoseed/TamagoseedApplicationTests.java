ae65c6efc417bcaa493561d59da0729bb0cc6a5c
João Lírio <joaovictorjgar@gmail.com>
2024-11-05 23:23:21 -0300

    Adjusting

--- src/test/java/com/tamagoseed/TamagoseedApplicationTests.java
+++ src/test/java/com/tamagoseed/TamagoseedApplicationTests.java
@@ -1,5 +1,6 @@
 package com.tamagoseed;
 
+<<<<<<< HEAD
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,3 +12,16 @@ class TamagoseedApplicationTests {
 	}
 
 }
+=======
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TamagoseedApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+}
+
+>>>>>>> 89cc5d6 (Atualizando para deploy)
