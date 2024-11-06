0980ddcd14f072729577ff4662373184bf9978e2
João Lírio <joaovictorjgar@gmail.com>
2024-11-05 23:54:11 -0300

    update

--- src/test/java/com/tamagoseed/TamagoseedApplicationTests.java
+++ src/test/java/com/tamagoseed/TamagoseedApplicationTests.java
@@ -1,18 +1,5 @@
 package com.tamagoseed;
 
-<<<<<<< HEAD
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TamagoseedApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
-=======
 //import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -24,4 +11,3 @@ public class TamagoseedApplicationTests {
 //    }
 }
 
->>>>>>> 89cc5d6 (Atualizando para deploy)
